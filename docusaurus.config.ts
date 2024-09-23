ec0a1beb160d9b7a03ddbb96299d710c4c356f0b
crashRT <crashrt.farbauti@gmail.com>
2024-09-23 13:07:11 +0900

    gh actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'crashRT', // Usually your GitHub org/user name.
   projectName: 'crashrt-memo', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
